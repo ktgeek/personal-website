648253fc46a931d03d711797543ead72b4afbd29
Keith T. Garner <kgarner@kgarner.com>
2026-05-19 09:49:09 -0500

    add ability to have draft posts

--- .eleventy.js
+++ .eleventy.js
@@ -54,6 +54,12 @@ module.exports = function(eleventyConfig) {
     return [...tagSet].sort();
   });
 
+  eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+    if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+      return false;
+    }
+  });
+
   return {
     pathPrefix: process.env.PATH_PREFIX || "/",
     dir: {
